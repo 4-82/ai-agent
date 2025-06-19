0a2ce6c7e8217b04e37505b38a7954ab9fac2cc9
4-82 <180662930+4-82@users.noreply.github.com>
2025-06-19 18:03:32 -0400

    main: add system_prompt

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
 def main():
     load_dotenv()
     args = sys.argv[1:]
